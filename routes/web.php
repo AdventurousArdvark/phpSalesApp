f0e8fc11410d29c729556f35d4e7de342e2f7a80
brett <brettlaguerra09@gmail.com>
2026-05-18 08:04:38 -0400

    Created Item and Customer Controllers and Requests;added routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('items', ItemController::class);
+    Route::resource('customers', CustomerController::class);
 });
 
 require __DIR__.'/auth.php';
